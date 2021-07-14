eff38a172405957a2c5df9d455004145396c7448
Ben Rudolph <36246631+benrudolphWSP@users.noreply.github.com>
2021-07-14 08:45:51 -0600

    Site cleanup and configuration

--- src/js/Components/NavSidebar.js
+++ src/js/Components/NavSidebar.js
@@ -2,33 +2,38 @@ const SidebarToggle = {
   setup: function () {
     SidebarToggle.setElements();
   },
+  getView: function () {
+    return window.matchMedia('(max-width: 1023px)').matches
+      ? 'mobile'
+      : 'desktop';
+  },
   setElements: function () {
-    SidebarToggle.menu = document.getElementById("mainNav");
-    SidebarToggle.mobileToggle = document.getElementById("mobileNavToggle");
+    SidebarToggle.menu = document.getElementById('mainNav');
+    SidebarToggle.mobileToggle = document.getElementById('mobileNavToggle');
     SidebarToggle.mobileToggleText = document.querySelector(
-      ".nav-mobile__toggle-text"
+      '.nav-mobile__toggle-text'
     );
-    SidebarToggle.menu.setAttribute("aria-hidden", true);
-    SidebarToggle.menu.setAttribute("hidden", "");
+    SidebarToggle.menu.setAttribute('aria-hidden', true);
+    SidebarToggle.menu.setAttribute('hidden', '');
     SidebarToggle.expanded = false;
-    document.addEventListener("click", SidebarToggle.clickHandler);
+    document.addEventListener('click', SidebarToggle.clickHandler);
   },
   clickHandler: function (event) {
-    if (!event.target.closest("#mobileNavToggle")) return;
+    if (!event.target.closest('#mobileNavToggle')) return;
     SidebarToggle.toggleSidebar();
   },
   toggleSidebar: function () {
     if (!SidebarToggle.expanded) {
-      SidebarToggle.mobileToggle.setAttribute("aria-expanded", true);
-      SidebarToggle.mobileToggle.classList.add("is-active");
-      SidebarToggle.menu.setAttribute("aria-hidden", false);
-      SidebarToggle.menu.removeAttribute("hidden");
+      SidebarToggle.mobileToggle.setAttribute('aria-expanded', true);
+      SidebarToggle.mobileToggle.classList.add('is-active');
+      SidebarToggle.menu.setAttribute('aria-hidden', false);
+      SidebarToggle.menu.removeAttribute('hidden');
       SidebarToggle.expanded = true;
     } else {
-      SidebarToggle.mobileToggle.setAttribute("aria-expanded", false);
-      SidebarToggle.mobileToggle.classList.remove("is-active");
-      SidebarToggle.menu.setAttribute("aria-hidden", true);
-      SidebarToggle.menu.setAttribute("hidden", "");
+      SidebarToggle.mobileToggle.setAttribute('aria-expanded', false);
+      SidebarToggle.mobileToggle.classList.remove('is-active');
+      SidebarToggle.menu.setAttribute('aria-hidden', true);
+      SidebarToggle.menu.setAttribute('hidden', '');
       SidebarToggle.expanded = false;
     }
   },
